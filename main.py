e05eb136762f6ed060a62ee64d6e7ed65002bdd5
sauravb <s.bhatta2383@gmail.com>
2024-11-13 01:23:07 -0800

    Allowed the testing for client communication

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-# main.py
 from fastapi import FastAPI
 from routers import transaction_router
 from models.models import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Include routers
 app.include_router(transaction_router.router, prefix="/transactions", tags=["transactions"])
 
 @app.get("/")
