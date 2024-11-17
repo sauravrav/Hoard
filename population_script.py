1580c32095a1ae1b0ed96a38f3e420f5b659d732
sauravb <s.bhatta2383@gmail.com>
2024-11-17 01:34:09 -0800

    Added demo user info without hashing

--- population_script.py
+++ population_script.py
@@ -9,19 +9,19 @@ try:
     bank2 = Bank(name="Wells Fargo Bank")
     session.add_all([bank1, bank2])
     
-    user1 = User(first_name="Ali", last_name="Khalilabadi", email="ali@sfbu", role="customer")
-    user2 = User(first_name="Niyan", last_name="Danny", email="niyan@danny", role="customer")
+    user1 = User(first_name="Ali", last_name="Khalilabadi", email="ali@sfbu", role="customer", password="aliali")
+    user2 = User(first_name="Danny", last_name="Tarafa", email="danny@sfbu", role="customer",password="dannydanny")
     session.add_all([user1, user2])
 
     session.commit()
 
     account11 = Account(account_type="savings", user_id=user1.id, bank_id=bank1.id, balance=1000)
-    account12 = Account(account_type="current", user_id=user1.id, bank_id=bank1.id, balance=500)
+    # account12 = Account(account_type="current", user_id=user1.id, bank_id=bank1.id, balance=500)
     
     account21 = Account(account_type="savings", user_id=user2.id, bank_id=bank2.id, balance=2000)
-    account22 = Account(account_type="current", user_id=user2.id, bank_id=bank2.id, balance=100)
+    # account22 = Account(account_type="current", user_id=user2.id, bank_id=bank2.id, balance=100)
     
-    session.add_all([account11, account12, account21, account22])
+    session.add_all([account11, account21])
 
     bank_user1 = BankUser(bank_id=bank1.id, user_id=user1.id)
     bank_user2 = BankUser(bank_id=bank2.id, user_id=user2.id)
