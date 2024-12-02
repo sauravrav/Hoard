21242bc2e818f327d19a222a24f7401c43c7091a
sauravb <s.bhatta2383@gmail.com>
2024-12-01 22:31:43 -0800

    Removed recent transaction response after login

--- routers/login_router.py
+++ routers/login_router.py
@@ -10,12 +10,9 @@ def login(email: str = Form(...), password: str = Form(...), db: Session = Depen
     try:
         user_query = text("SELECT * FROM users WHERE email = :email")
         user = db.execute(user_query, {"email": email}).fetchone()
-
         if not user:
             raise HTTPException(status_code=401, detail="Invalid email or password")
-
         user_dict = dict(user._mapping)
-
         if user_dict["encrypted_password"] != password:
             raise HTTPException(status_code=401, detail="Invalid email or password")
 
@@ -26,7 +23,6 @@ def login(email: str = Form(...), password: str = Form(...), db: Session = Depen
         """)
         user_accounts = db.execute(accounts_query, {"user_id": user_dict["id"]}).fetchall()
         account_ids = [account[0] for account in user_accounts]
-
         if not account_ids:
             return {
                 "message": "Login successful!",
@@ -38,33 +34,6 @@ def login(email: str = Form(...), password: str = Form(...), db: Session = Depen
                     "banks": []
                 }
             }
-
-        transactions_query = text(f"""
-            SELECT id, 
-                   CASE 
-                       WHEN target_account_id IN :account_ids THEN 'credit' 
-                       ELSE 'debit' 
-                   END AS type, 
-                   amount, 
-                   timestamp 
-            FROM transactions 
-            WHERE source_account_id IN :account_ids OR target_account_id IN :account_ids
-            ORDER BY timestamp DESC
-            LIMIT 5
-        """)
-        recent_transactions = db.execute(transactions_query, {"account_ids": tuple(account_ids)}).fetchall()
-        # breakpoint()
-        
-        transactions_data = [
-            {
-                "id": transaction[0],
-                "type": transaction[1],
-                "amount": transaction[2],
-                "timestamp": transaction[3]
-            }
-            for transaction in recent_transactions
-        ]
-
         banks_query = text("""
             SELECT b.id AS bank_id, 
                    b.name AS bank_name, 
@@ -93,11 +62,10 @@ def login(email: str = Form(...), password: str = Form(...), db: Session = Depen
         return {
             "message": "Login successful!",
             "token": user_dict["email"],
-            "password": user_dict["password"],
+            "password": user_dict["encrypted_password"],
             "user": {
                 "name": f"{user_dict['first_name']} {user_dict['last_name']}",
                 "email": user_dict["email"],
-                "recent_transactions": transactions_data,
                 "banks": banks_data,
             }
         }
